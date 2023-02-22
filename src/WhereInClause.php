1250ed45cb6134e0aaea82b47b52bdcacb1b5fff
mateuszkorbanek <mateusz.korbanek@amsterdamstandard.com>
2023-02-22 21:35:39 +0100

    Added subquery support for in query

--- src/WhereInClause.php
+++ src/WhereInClause.php
@@ -1,24 +1,34 @@
 <?php
+
 namespace WPQueryBuilder;
 
-class WhereInClause implements WhereClause {
+class WhereInClause implements WhereClause
+{
+    private $column;
+    private $value1;
+    private $value2;
 
-	private $column;
-	private $value1;
-	private $value2;
+    public function __construct($column, $values)
+    {
+        $this->column = $column;
+        $this->bindings = $values;
+    }
 
-	public function __construct($column, $values){
-		$this->column = $column;
-		$this->bindings = $values;
-	}
+    public function buildSql()
+    {
+        if (strtolower(substr($this->bindings[0], 0, 6)) == 'select') {
+            $subquery = $this->bindings[0];
 
-	public function buildSql() {
-		$inList = '(' . implode(', ', array_fill(0, count($this->bindings), '%s')) . ')';
-		return implode(' ', [$this->column, "IN", $inList]);
-	}
+            return implode(' ', [$this->column, 'IN', "($subquery)"]);
+        }
 
-	public function getBindings(){
-		return $this->bindings;
-	}
+        $inList = '('.implode(', ', array_fill(0, count($this->bindings), '%s')).')';
 
+        return implode(' ', [$this->column, 'IN', $inList]);
+    }
+
+    public function getBindings()
+    {
+        return $this->bindings;
+    }
 }
